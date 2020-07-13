5c379cb0e5a97fcbeae38b8df756e219523e1456
seoa <andrew.d.seo@gmail.com>
2020-07-13 15:14:24 -0700

    created grid, added wall-making functionality

--- frontend/graphsearch_algos/grid.jsx
+++ frontend/graphsearch_algos/grid.jsx
@@ -1,14 +1,57 @@
 import React from 'react';
+import Square from './square';
 
 export default class Grid extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            make_wall: false,
+            remove_wall: false,
+            click_wall: false
+        }
+
+        this.handle_mouse_up = this.handle_mouse_up.bind(this);
+        this.handle_mouse_down = this.handle_mouse_down.bind(this);
+        this.change_click_state = this.change_click_state.bind(this);
+        this.sleep = this.sleep.bind(this);
+    }
+    sleep(msec) {
+        return new Promise(resolve => setTimeout(resolve, msec));
+    }
+
+    async handle_mouse_down() {
+        await this.sleep(0);
+        if(this.state.click_wall) {
+            this.setState({ remove_wall: true });
+        }
+        else {
+            this.setState({ make_wall: true });
+        }
+    }
+
+    handle_mouse_up() {
+        this.setState({ make_wall: false, remove_wall: false });
+    }
+    change_click_state(is_wall) {
+        if(is_wall) {
+            this.setState({ click_wall: true });
+        }
+        else {
+            this.setState({ click_wall: false });
+        }
     }
 
     render() {
+        let render_arr = [];
+        for(let i=0; i<1300; i++) {
+            render_arr.push(<Square key={i} 
+                make_wall={this.state.make_wall}
+                change={this.change_click_state}
+                remove_wall={this.state.remove_wall}/>);
+        }
         return (
-            <div className="grid">
-
+            <div className="grid" onMouseDown={this.handle_mouse_down} onMouseUp={this.handle_mouse_up}>
+                {render_arr}
             </div>
         )
     }
--- frontend/graphsearch_algos/square.jsx
+++ frontend/graphsearch_algos/square.jsx
@@ -3,11 +3,42 @@ import React from 'react';
 export default class Square extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            wall: false
+        }
+        this.handle_mouse_over = this.handle_mouse_over.bind(this);
+        this.handle_click = this.handle_click.bind(this);
+        this.sleep = this.sleep.bind(this);
+    }
+    sleep(msec) {
+        return new Promise(resolve => setTimeout(resolve, msec));
+    }
+    handle_mouse_over() {
+        if(this.props.make_wall) {
+            this.setState({ wall: true });
+        }
+        if(this.props.remove_wall) {
+            this.setState({ wall: false });
+        }
+    }
+
+    async handle_click() {
+        await this.props.change(this.state.wall);
+        if(this.state.wall) {
+            this.setState({ wall: false });
+        }
+        else {
+            this.setState({ wall: true });
+        }
     }
 
     render() {
+        let add_class = "";
+        if(this.state.wall) {
+            add_class += " wall";
+        }
         return( 
-            <div className="square">
+            <div className={`square${add_class}`} onMouseDown={this.handle_click} onMouseOver={this.handle_mouse_over}>
 
             </div>
         )
